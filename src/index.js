5339204f8d3305059adff9a5270e2ae286ad162d
Alv0425 <vilejshikova@gmail.com>
2023-08-10 15:27:41 +0300

    feat: add variables and check type of input

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ module.exports = function toReadable (num) {
     //start HRN task
     const numbers019 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     const numbers20100 = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let hrnum = '';
+    if (typeof(num) != 'number') {
+        return hrnum = 'Error: cannot define number';
+    }
+    let numstr = num + '';
 }
